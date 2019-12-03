bb037e18397d673b67345db10c193079947babcf
Jayanth Kannan <jayanth@intercake.com>
2019-12-03 09:12:57 +0000

    first commit

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('managed-73ecb9722d049f6c9f057eb87b7f6eb5c934d994').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed-73ecb9722d049f6c9f057eb87b7f6eb5c934d994
